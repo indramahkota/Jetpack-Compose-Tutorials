ebfefd4e6df88c9d2e6b7faaaa5aa5dcf4255baa
Smart Tool Factory <smarttoolfactory@icloud.com>
2026-01-06 22:52:53 +0300

    remove unused imports

--- Tutorial4-1ChatBot/src/main/java/com/smarttoolfactory/tutorial4_1chatbot/samples/TrailFadeInTextWithCallback.kt
+++ Tutorial4-1ChatBot/src/main/java/com/smarttoolfactory/tutorial4_1chatbot/samples/TrailFadeInTextWithCallback.kt
@@ -1,8 +1,6 @@
 package com.smarttoolfactory.tutorial4_1chatbot.samples
 
 import android.widget.Toast
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
@@ -25,9 +23,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.CompositingStrategy
 import androidx.compose.ui.graphics.drawscope.ContentDrawScope
@@ -35,7 +30,6 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
