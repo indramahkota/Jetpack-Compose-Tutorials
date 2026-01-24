92c756d170b5b4d1630c41e97e4f9e6ced0fcbb5
Smart Tool Factory <smarttoolfactory@icloud.com>
2026-01-24 12:36:23 +0300

    fix JumpToBottomButton shadow flickering

--- Tutorial4-1ChatBot/src/main/java/com/smarttoolfactory/tutorial4_1chatbot/ui/component/button/JumpToBottomButton.kt
+++ Tutorial4-1ChatBot/src/main/java/com/smarttoolfactory/tutorial4_1chatbot/ui/component/button/JumpToBottomButton.kt
@@ -4,20 +4,54 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDownward
+import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@Preview
+@Composable
+fun JumpToBottomButtonPreview() {
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+    ) {
+
+        var enabled by remember {
+            mutableStateOf(false)
+        }
+
+        Button(
+            onClick = {
+                enabled = enabled.not()
+            }
+        ) {
+            Text("Enabled: $enabled")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        JumpToBottomButton(enabled = enabled) {}
+    }
+}
 
 @Composable
 internal fun JumpToBottomButton(
@@ -29,20 +63,15 @@ internal fun JumpToBottomButton(
     AnimatedVisibility(
         modifier = modifier,
         visible = enabled,
-        enter = fadeIn(animationSpec = tween(500)),
-        exit = fadeOut(animationSpec = tween(500))
+        enter = fadeIn(animationSpec = tween(1000)),
+        exit = fadeOut(animationSpec = tween(1000))
     ) {
         FloatingActionButton(
-            elevation = FloatingActionButtonDefaults.elevation(
-                defaultElevation = 4.dp,
-                pressedElevation = 8.dp
-            ),
             shape = CircleShape,
             containerColor = Color.White,
             modifier = Modifier
-                .padding(end = 16.dp)
-                .size(48.dp)
-                .shadow(elevation = 2.dp, shape = CircleShape),
+                .padding(16.dp)
+                .size(48.dp),
             onClick = onClick
         ) {
             Icon(
